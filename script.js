4bae8f5c92d44b8048f76035baeecb1d823b250a
kylengreene <kylengreene@gmail.com>
2020-01-31 16:54:27 -0600

    inputs all tracking through and adding new employee to array.

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ function onReady(){
     $('#submit').on('click',submitEmployee);
 }
 
+let employeeRoster = [];
+
 function submitEmployee(){
     console.log('in submit employee');
+    let newEmployee = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        idNumber: $('#idIn').val(),
+        jobTitle: $('#jobTitleIn').val(),
+        annualSalary: $('#annualSalaryIn').val()
+    }
+    employeeRoster.push(newEmployee);
+    
+    console.log(employeeRoster);
+    
     
 }
