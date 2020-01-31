6e985ebbf3be363af790df924a3f157dc51f376b
kylengreene <kylengreene@gmail.com>
2020-01-31 17:04:07 -0600

    employee object pusing through to array and being displayed on the DOM.

--- script.js
+++ script.js
@@ -16,8 +16,21 @@ function submitEmployee(){
         annualSalary: $('#annualSalaryIn').val()
     }
     employeeRoster.push(newEmployee);
-    
+
     console.log(employeeRoster);
+    displayEmployees();
     
+}
+
+function displayEmployees () {
+    let el = $('#employeesOut');
+    
+    el.empty();
+
+    for (let i = 0; i < employeeRoster.length; i++){
+        el.append(`<li> ${employeeRoster[i].firstName} ${employeeRoster[i].lastName} : ${employeeRoster[i].idNumber}  ${employeeRoster[i].jobTitle} $${employeeRoster[i].annualSalary}`);
+    }
+
+
     
 }
